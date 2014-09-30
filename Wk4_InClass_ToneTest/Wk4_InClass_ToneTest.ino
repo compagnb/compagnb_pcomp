ff9f32d679dd939e6936757f7b329add07dafb63
Barbara Compagnoni <compagnb@newschool.edu>
2014-09-30 10:54:46 -0400

    Wk 4 InClass

---
+++ Wk4_InClass_ToneTest/Wk4_InClass_ToneTest.ino
@@ -0,0 +1,18 @@
+//Barbara Compagnoni & Nadine Razzouk
+//P-Comp 
+//Fall 2014
+//In Class Assignment : Tones 1
+
+void setup() {
+    // nothing to do here
+ }
+ 
+ void loop() {
+   // get a sensor reading:
+   int sensorReading = analogRead(A0);
+   // map the results from the sensor reading's range
+   // to the desired pitch range:
+   float frequency = map(sensorReading, 200, 900, 100, 1000);
+   // change the pitch, play for 10 ms:
+   tone(8, frequency, 10);
+ }
